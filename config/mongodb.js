110268a552e10ba7badffbb9b66be225d45e2a8e
Rick Voermans <rrw.voermans@student.avans.nl>
2017-12-18 16:30:43 +0100

    Opstart server

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,4 +3,11 @@ const config = require('./env/env');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(congif.dburl)
+mongoose.connect(congif.dburl);
+var connection  = mongoose.connection
+    .once('open', () => console.log('Geconnect met mongo: ' + config.dburl))
+    .on('error', (error) => {
+        console.warn('Warning', error.toString());
+    });
+
+module.exports = connection;
