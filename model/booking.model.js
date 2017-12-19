f4bee9304e38acc7e976f2b515a9fb974b244c57
Rick Voermans <rrw.voermans@student.avans.nl>
2017-12-19 11:15:47 +0100

    booking model toegevoegd

--- model/booking.model.js
+++ model/booking.model.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const SportsHallField = require('./sportshallfield.model.js');
 
 const BookingSchema = new Schema({
-    //Booking schema
+    day: {
+        type: Date,
+        required: true
+    },
+    startingTime: {
+        type: Date,
+        required = true
+    },
+    endingTime: {
+        type: Date,
+        required: true
+    },
+    userId: {
+        type: Number,
+        required: true
+    },
+    sportsHallField: SportsHallField
 });
 
 const Booking = mongoose.model('booking', BookingSchema);
