d4f94797ccab8b126b15dd5a393bc89cf2d9f52b
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-12-19 10:32:13 +0100

    User model af, voor nu

--- model/user.model.js
+++ model/user.model.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    //User schema
+    email: {type:String,required:true},
+    username: {type:String,required:true},
+    password: {type:String,required:true}
 });
 
 const User = mongoose.model('user', UserSchema);
