3f7fbfaa86c1f634b1eb18108ac9994534d52263
Rick Voermans <rrw.voermans@student.avans.nl>
2017-12-19 13:53:16 +0100

    post request

--- api/booking.routes.js
+++ api/booking.routes.js
@@ -5,12 +5,22 @@ const Booking = require('../model/booking.model');
 
 //Ophalen van alle boekingen:
 routes.get('', function(req, res) {
-    
+
 });
 
 //Boeking toevoegen:
 routes.post('', function(req, res) {
+    res.contentType('application/json');
 
+    const bookingBody = req.body;
+    const booking = new Booking(bookingBody);
+
+    booking.save()
+        .then(() => {
+            res.status(200).json(booking);
+        }).catch((error) => {
+            res.status(400).json(error);
+    });
 });
 
 module.exports = routes;
