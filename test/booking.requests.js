d674393d03fed72015cf4132c325e8e5c846f146
Rick Voermans <rrw.voermans@student.avans.nl>
2017-12-22 13:13:59 +0100

    POST Test bijna klaar

--- test/booking.requests.js
+++ test/booking.requests.js
@@ -8,8 +8,37 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Booking requests', () => {
+    var booking;
+
+    beforeEach((done) => {
+        booking = new Booking({
+            day: '2017-12-27T12:50:23.452',
+            startingTime: '11:00',
+            endingTime: '12:00',
+            userId: '54e6392e-6d8a-4436-8e91-a2c5daf0b660',
+            SportsHallField: 1,
+            status: 0
+        });
+
+        booking.save().then(() => done());
+    });
+
     it('Can post a booking to the NoSQL server', (done) => {
-        done();
+        chai.request(server)
+            .post('/api/bookings')
+            .send(booking)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+
+                //properties the body should have:
+                res.body.booking.should.have.property('day');
+                res.body.booking.should.have.property('startingTime');
+                res.body.booking.should.have.property('endingTime');
+                res.body.booking.should.have.property('userId');
+                res.body.booking.should.have.property('status');
+                done();
+        });
     });
 
     it('Can post a booking to the SQL server VIA the NoSQL server', (done) => {
@@ -26,6 +55,6 @@ describe('Booking requests', () => {
                 chai.assert.equal(res.body.error,undefined);
                 
                 done();
-            });
+        });
     });
 });
