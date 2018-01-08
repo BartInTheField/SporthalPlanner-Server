b6058c25bb5ea9540de80be34aea7393d1259e2e
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-01-08 12:22:33 +0100

    GET Route toegevoegd voor 1 enkele sportsfacility

--- api/sportsfacility.routes.js
+++ api/sportsfacility.routes.js
@@ -3,10 +3,19 @@ var express = require('express');
 var routes = express.Router();
 
 const SportsFacility = require('../model/sportsfacility.model');
+const API = require('../config/api_requester');
 
 //Closingdays opvragen:
-routes.get('', function(req, res) {
+routes.get('/:id', function(req, res) {
+  const id = req.params.id;
 
+  API.request('/api/sportsfacilities/'+id, 'GET', {}, (response) => {
+    if (response.error) {
+      res.status(400).json({ error: 'Could not retrieve Sportsfacility' });
+    } else {
+      res.status(200).json(response);
+    }
+  });
 });
 
 //Closingday toevoegen:
