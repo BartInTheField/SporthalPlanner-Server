b6ced0f30c4cb5abc37324e3738751c99cc24a01
Rick Voermans <rrw.voermans@student.avans.nl>
2018-01-08 11:22:18 +0100

    model aangemaakt voor sluitingstijden

---
+++ model/closingdays.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+const SportsFacility = require('../model/sportsfacility.model');
+
+const ClosingDaysSchema = new Schema({
+    date: {
+        type: Date,
+        required: [true, 'Date is required.']
+    },
+    reason: String,
+});
+
+module.exports = ClosingDaysSchema;
