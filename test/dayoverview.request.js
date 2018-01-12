acb5eedb1fca2284f7acd29985cad4d9a850793a
Bart in 't Veld <bart.int.007@gmail.com>
2018-01-12 14:15:09 +0100

    dayoverview test

---
+++ test/dayoverview.request.js
@@ -0,0 +1,74 @@
+const server = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const Customer = require('../model/customer.model');
+const SportsFacility = require('../model/sportsfacility.model');
+
+describe('Dayoverview test: ', function () {
+  let customer;
+  let customerId;
+  let sportsfacility;
+  let sportsfacilityId;
+  let date = '20021010'
+
+  customer = new Customer({
+    'userId':'a',
+    'sporthalHurenUsername':'Henkie420',
+    'sporthalHurenUserId':'',
+    'firstName':'Henk',
+    'lastName':'Henkerson',
+    'isSporthalHurenCustomer':false
+  })
+
+  sportsfacility = new SportsFacility({
+    "openingHours": {
+      "openingHoursId": 4,
+      "monday": "7,00;21,00",
+      "tuesday": "7,00;21,00",
+      "wednesday": "7,00;21,00",
+      "thursday": "0,00;0,00",
+      "friday": "7,00;21,00",
+      "saturday": "7,00;15,00",
+      "sunday": "0,00;0,00"
+    },
+    "email": "Mooiesporthaltehuur@email.nl",
+    "phone": "612345678",
+    "city": "Engeland",
+    "address": "Straat 3",
+    "name": "Sportzone",
+    "sportsHalls": []
+  })
+
+  afterEach((done) => {
+    Customer.findByIdAndRemove(customerId)
+      .then(() =>
+        SportsFacility.findByIdAndRemove(sportsfacilityId)
+          .then(()=>{
+            done();
+          }))
+  });
+
+  it('Can get a day overview', (done) => {
+    chai.request(server)
+      .post('/api/customers/')
+      .send(customer)
+      .end((err,res)=>{
+        customerId = res.userId;
+        chai.request(server)
+          .post('/api/sportsfacility/')
+          .send(sportsfacility)
+          .end((err,res) => {
+            sportsfacilityId = res.body._id;
+            chai.request(server)
+              .get('/api/dayoverview/'+customerId+'/'+sportsfacilityId+'/'+date)
+              .end((err,res) => {
+                res.should.have.status(200);
+                done();
+              })
+          })
+
+      })
+  })
+
+})
