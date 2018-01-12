073cbb9d661b2e0af61fa2dc9365ebb20ec3c32c
Bart in 't Veld <bart.int.007@gmail.com>
2018-01-12 13:27:51 +0100

    Customer out of sporthalhuren

--- api/customer.routes.js
+++ api/customer.routes.js
@@ -21,17 +21,49 @@ routes.post('', function(req, res) {
     });
 });
 
+function updateMongoWithSporthalHurenCustomers(userId) {
+    return new Promise(function(resolve,reject) {
+        API.request('/api/bookings', 'GET', {}, (response) => {
+            if (response.error) {
+                console.log(response);
+                reject();
+            } else {
+                response.forEach(booking => {
+                    if (userId == booking._embedded.SportsFacility.userId) {
+                        Customer.find({sporthalHurenUserId: booking.userId})
+                            .then((customers) => {
+                                if (customers.length === 0) {
+                                    const newCustomer = new Customer({isSporthalHurenCustomer: true, sporthalHurenUsername: 'null', sporthalHurenUserId: booking.userId, userId: userId})
+                                    newCustomer.save();
+                                }
+                            })
+                    }
+                });
+                resolve();
+            }
+        });
+    });
+}
+
 //Retrieve customer data from a user
 routes.get('/:userid', function(req, res) {
     const paramUserId = req.params.userid;
-    Customer.find({ userId : paramUserId})
-        .then((users) => {
-            res.status(200).json(users);
+
+    updateMongoWithSporthalHurenCustomers(paramUserId)
+        .then(() => {
+            Customer.find({ userId : paramUserId})
+                .then((users) => {
+                    res.status(200).json(users);
+                })
+                .catch(error => {
+                    res.status(401).json({message:'Error'})
+                    console.log(error);
+                });
         })
-        .catch(error => {
-            res.status(401).json({message:'Error'})
-            console.log(error);
-        });
+        .catch(() => {
+            res.status(400)
+        })
+
 });
 
 //Update customer data from a customer id
