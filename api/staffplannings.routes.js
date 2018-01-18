00768386cdb84215167c7586d762931063c836d7
Felix Boons <felixboons@hotmail.com>
2018-01-18 18:53:19 +0100

    hotfix POST for Planning returns whole StaffMember object

--- api/staffplannings.routes.js
+++ api/staffplannings.routes.js
@@ -8,14 +8,22 @@ const API = require('../config/api_requester');
 //Add staffplanning:
 routes.post('', function(req, res) {
     res.contentType('application/json');
-    const staffPlanning = req.body;
 
-    //POST a staffplanning to MongoDB
-    const newStaffPlanning = new StaffPlanning(staffPlanning);
+    // Create a staff planning object
+    const newStaffPlanning = new StaffPlanning({
+        day: req.body._day,
+        startingTime: req.body._startingTime,
+        endingTime: req.body._endingTime,
+        sportsFacility: req.body._sportsFacilityId,
+        staffMember: req.body._staffMember
+    });
 
+    //Save new staff planning to MongoDB
     newStaffPlanning.save()
         .then(() => {
-            res.status(200).json(newStaffPlanning);
+            res.status(200).json({
+                planning: newStaffPlanning,
+                staffMember: req.body.staffMemberObject});
         }).catch((error) => {
             res.status(400).json(error);
     });
@@ -24,7 +32,7 @@ routes.post('', function(req, res) {
 //Get staffplanning using sportsfacility ID:
 routes.get('/sportsfacilities/:sportsfacilityId', function(req, res) {
     res.contentType('application/json');
-    const sportsfacilityParam = req.params.sportsfacilityId
+    const sportsfacilityParam = req.params.sportsfacilityId;
 
     StaffPlanning.find({sportsFacility: sportsfacilityParam})
         .then((plannings) => {
